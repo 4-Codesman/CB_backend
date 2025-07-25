d85a932f0165008094f0efe0e1d3b34f22a02096
Zayd Fazel-Ellahi <zaydellahi@gmail.com>
2025-07-25 19:56:14 +0200

    Mongo Connection v1

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 require('dotenv').config();
+const mongoose = require('mongoose');
 const app = require('./app');
-
 
 const port = process.env.PORT || 5000;
 
+// Start the server
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
 
+// MongoDB connection
+const mongoURI = process.env.MONGO_URI;
+mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log('Connected to MongoDB');
+  })
+  .catch((err) => {
+    console.error('Error connecting to MongoDB:', err);
+  });

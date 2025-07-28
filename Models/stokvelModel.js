1006f92201cf453c661b6dddb18eeed4c40648d0
Zayd Fazel-Ellahi <zaydellahi@gmail.com>
2025-07-29 01:30:49 +0200

    stokvelModel Update

--- Models/stokvelModel.js
+++ Models/stokvelModel.js
@@ -3,7 +3,11 @@ const Schema = mongoose.Schema;
 
 const stokvelSchema = new Schema({
     _id: Schema.Types.ObjectId,
-    name: String
+    name: String, required: true,
+    type: Number, required: true, // 0 for savings, 1 for investment
+    amount: Number, required: true,
+    period: String, required: true,
+    date: Date, required: true
 })
 
 const Stokvel = mongoose.model('Stokvel', stokvelSchema, 'Stokvels');

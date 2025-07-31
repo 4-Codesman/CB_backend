47d4860f236542509e9b9078ac7df8ada40c952e
Zayd Fazel-Ellahi <zaydellahi@gmail.com>
2025-07-31 20:09:03 +0200

    Update GET for viewing stokvels/invites

--- Controllers/JStokvelController.js
+++ Controllers/JStokvelController.js
@@ -8,15 +8,19 @@ exports.checkRequestsByEmail= async (req, res) => {
         return res.status(400).json({ error: 'Email not stored in memory'});
       }
   
-      const user = await User.find({ user_email:email, status:0 });
+      const user = await User.find({ user_email:email, status:0 }).populate('sv_id', 'name');
   
       if (!user || user.length === 0) {
         return res.status(200).json({ stokvels: [] });
       }
 
-      const stokvelIds = user.map(userDoc => userDoc.sv_id);
+      const stokvels = user.filter(u => u.sv_id && u.sv_id.name) // safeguard
+      .map(u => ({
+        id: u.sv_id._id,
+        name: u.sv_id.name
+      }));
   
-      res.json({ stokvels: stokvelIds });
+      res.json({ stokvels});
     } catch (err) {
       console.error(err);
       res.status(500).json({ error: 'Server error' });

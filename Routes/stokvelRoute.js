1e5b9e88eab0487333be823da67e36ca1a9ea279
Zayd Fazel-Ellahi <zaydellahi@gmail.com>
2025-08-01 02:12:53 +0200

    Join Popup fix attempt

--- Routes/stokvelRoute.js
+++ Routes/stokvelRoute.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const Stokvel= require('../Controllers/CStokvelController');
+const JStokvel=require('../Controllers/JStokvelController');
 
 router.post('/create', Stokvel.createStokvel);
+router.get('/details/:id', JStokvel.getStokvelDetails);
 
 module.exports = router;
 

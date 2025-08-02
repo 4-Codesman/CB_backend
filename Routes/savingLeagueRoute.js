81357c051dc18da90f136158b8d64de12460a084
Ayanda-Bradley-Mzizi <2688042@students.wits.ac.za>
2025-08-02 22:53:29 +0200

    fixed savings league routes

--- Routes/savingLeagueRoute.js
+++ Routes/savingLeagueRoute.js
@@ -3,11 +3,14 @@ const router = express.Router();
 const savingLeagueController = require('../Controllers/savingLeagueController');
 
 // POST /api/saving-leagues/create
-router.post('/create', savingLeagueController.createSavingLeague);
-router.get('/:leagueId', savingLeagueController.getSavingLeagueById);
-router.post('/:leagueId/join', savingLeagueController.joinSavingLeague);
-router.get('/uID/:uID', savingLeagueController.getUserLeagues);
+// ✅ Specific first
 router.get('/open', savingLeagueController.getOpenSavingLeagues);
+router.get('/uID/:uID', savingLeagueController.getUserLeagues);
+
+// ✅ Generic after
+router.post('/:leagueId/join', savingLeagueController.joinSavingLeague);
+router.get('/:leagueId', savingLeagueController.getSavingLeagueById);
+
 /*
 router.get('/:leagueId', savingLeagueController.getSavingLeagueById);
 */

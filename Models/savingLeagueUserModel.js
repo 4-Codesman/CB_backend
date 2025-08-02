13674867fafd23455d1dbbba9f5136f9d43e6514
Ayanda-Bradley-Mzizi <2688042@students.wits.ac.za>
2025-08-02 17:33:57 +0200

    Update savingLeagueUserModel.js

--- Models/savingLeagueUserModel.js
+++ Models/savingLeagueUserModel.js
@@ -1,12 +1,11 @@
 const mongoose = require('mongoose');
-const Schema = mongoose.Schema; 
+const Schema = mongoose.Schema;
 
 const savingLeagueUserSchema = new Schema({
-    svl_id: { type: Schema.Types.ObjectId, ref: 'SavingLeague', required: true },
-    user_id: { type: Schema.Types.ObjectId, ref: 'User', required: true },
-    goal: { type: Number, required: true }
-
-})
+  svl_id: { type: Schema.Types.ObjectId, ref: 'SavingLeague', required: true },
+  user_id: { type: String, required: true }, // ✅ Changed from ObjectId to String to store Firebase UID
+  goal: { type: Number, required: true }
+});
 
 const SavingLeagueUser = mongoose.model('SavingLeagueUser', savingLeagueUserSchema, 'SavingLeagueUsers');
 module.exports = SavingLeagueUser;

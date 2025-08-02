7a67cb2f3b62e45788c6cc821686367a84f5238f
Ayanda-Bradley-Mzizi <2688042@students.wits.ac.za>
2025-08-02 21:38:55 +0200

    get open savingLeagueRoute

--- Routes/savingLeagueRoute.js
+++ Routes/savingLeagueRoute.js
@@ -7,6 +7,7 @@ router.post('/create', savingLeagueController.createSavingLeague);
 router.get('/:leagueId', savingLeagueController.getSavingLeagueById);
 router.post('/:leagueId/join', savingLeagueController.joinSavingLeague);
 router.get('/uID/:uID', savingLeagueController.getUserLeagues);
+router.get('/open', savingLeagueController.getOpenSavingLeagues);
 /*
 router.get('/:leagueId', savingLeagueController.getSavingLeagueById);
 */
